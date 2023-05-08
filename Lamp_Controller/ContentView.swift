dcb57b9e2af1ace04f9d1b96826c868bafb07ad1
ahmed fraag <ahmedfraag255@gmail.com>
2023-05-08 10:15:07 +0300

    Initial Commit

---
+++ Lamp_Controller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lamp_Controller
+//
+//  Created by ahmed fraag on 08/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lamp_Controller/Lamp_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lamp_ControllerApp.swift
+//  Lamp_Controller
+//
+//  Created by ahmed fraag on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lamp_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
